84b463df998cd36237427a4b38aaf4ff17e30213
SapphireKing810 <78891918+SapphireKing810@users.noreply.github.com>
2022-09-18 01:26:34 -0400

    Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String[] prototypeInputPaths = {"prototype-ordered.txt", "prototype-reverse.txt", "prototype-random01.txt"};
+        String[] prototypeInputPaths = {"prototype-ordered.txt", "prototype-reverse.txt", "prototype-random02.txt"};
         String[] largeInputPaths = {"large-ordered.txt", "large-reverse.txt", "large-random01.txt"};
+        int prototypeDataLength = 16;
+        int largeDataLength = 2000;
 
-        int[]   prototypeWorstCase = new int[16];
-        int[]   prototypeBestCase = new int[16];
-        int[]   prototypeAverageCase = new int[16];
+        int[]   prototypeWorstCase = new int[prototypeDataLength];
+        int[]   prototypeBestCase = new int[prototypeDataLength];
+        int[]   prototypeAverageCase = new int[prototypeDataLength];
 
-        int[]   largeWorstCase = new int[2000];
-        int[]   largeBestCase = new int[2000];
-        int[]   largeAverageCase = new int[2000];
+        int[]   largeWorstCase = new int[largeDataLength];
+        int[]   largeBestCase = new int[largeDataLength];
+        int[]   largeAverageCase = new int[largeDataLength];
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < prototypeInputPaths.length; i++){
             File file = new File(prototypeInputPaths[i]);
             Scanner scanner = new Scanner(file);
-            for (int j = 0; j < 16; j++){
+            for (int j = 0; j < prototypeDataLength; j++){
                 if(i == 0){
                     if (j == 0)
                         System.out.println("Best case data set: ");
@@ -36,14 +38,14 @@ public class Main {
                     prototypeAverageCase[j] = scanner.nextInt();
                     System.out.println("Index: " + j + " number: " + prototypeAverageCase[j]);
                 }
-
             }
             System.out.println("");
         }
-        for (int i = 0; i < 3; i++){
+
+        for (int i = 0; i < largeInputPaths.length; i++){
             File file = new File(largeInputPaths[i]);
             Scanner scanner = new Scanner(file);
-            for (int j = 0; j < 2000; j++){
+            for (int j = 0; j < largeDataLength; j++){
                 if(i == 0){
                     if (j == 0)
                         System.out.println("Best case data set: ");
@@ -153,7 +155,7 @@ public class Main {
         int temp, comparisons = 0, exchanges = 0, n = array.length;
         boolean isSorted;
 
-        for(int i = 1; i != n; i++){
+        for(int i = 1; i < n; i++){
             isSorted = true;
             for(int j = 0; j < n-i; j++){
                 comparisons++;
@@ -184,14 +186,14 @@ public class Main {
         //Worst Case    |   O(n^2)      |   O(n^2)
         //Avg Case      |   O(n^2)      |   O(n^2)
         int temp, comparisons = 0, exchanges = 0, bestCaseComparison = 0;
+        int n = array.length;
         System.out.print("Unsorted Array: ");
         for (int k : array) System.out.print(k + " ");
         System.out.println("");
 
-        for(int i = 2; i < array.length; i++){
+        for(int i = 1; i < n; i++){
             temp = array[i];
             int j = i - 1;
-            bestCaseComparison++;
             while(j >= 0 && array[j] > temp){
                 array[j + 1] = array[j];
                 j--;
@@ -205,8 +207,6 @@ public class Main {
                 exchanges);
         System.out.println("Insertion Sort: Number of Comparisons: " +
                 comparisons);
-        System.out.println("Insertion Sort: Number of Comparisons: " +
-                bestCaseComparison);
         System.out.println ("Sorted file:");
         for (int j : array) System.out.print(j + " ");
         System.out.println ();
